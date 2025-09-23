8f465e8c14b64e167c8d6581e4b7c45e69582bb2
M-Abubakar-Ansari <muhammadabubakarsiddiqueansari@gmail.com>
2025-09-23 20:22:05 +0500

    ...

---
+++ database/migrations/2025_09_23_145423_create_notes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->text("content"); // Content of note
+            $table->string("context"); // Can be for "proj" or "task"
+            $table->string("context_id"); // Task or project id
+            $table->unsignedBigInteger("member_id"); // member id
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
